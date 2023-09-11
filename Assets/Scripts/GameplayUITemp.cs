ef8fa222656f61b6efe567123e029ad37fc1a62e
Jonathan Aronsson Olsson <jonte962010@hotmail.com>
2023-09-11 21:41:11 +0200

    Added Unity & some work

---
+++ Assets/Scripts/GameplayUITemp.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameplayUITemp : MonoBehaviour
+{
+    public TextMeshProUGUI leftEngineValue;
+    public TextMeshProUGUI rightEngineValue;
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayController : MonoBehaviour
+{
+    public Rigidbody leftRigidbody;
+    public Rigidbody rightRigidbody;
+
+    public Slider leftSliderControl;
+    public Slider rightSliderControl;
+
+    private float leftEngineThrust = 0;
+    private float rightEngineThrust = 0;
+    private float thrustValue = 5;
+    private GameplayUITemp ui;
+
+    private void Start()
+    {
+        ui = FindObjectOfType<GameplayUITemp>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            leftEngineThrust += thrustValue * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            leftEngineThrust -= thrustValue * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            rightEngineThrust += thrustValue * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rightEngineThrust -= thrustValue * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            leftEngineThrust = 0;
+            rightEngineThrust = 0;
+        }
+
+        //leftEngineThrust = leftSliderControl.value;
+        //rightEngineThrust = rightSliderControl.value;
+
+        leftEngineThrust = Mathf.Clamp(leftEngineThrust, -10, 10);
+        rightEngineThrust = Mathf.Clamp(rightEngineThrust, -10, 10);
+
+        leftRigidbody.velocity = leftRigidbody.transform.up * leftEngineThrust;
+        rightRigidbody.velocity = rightRigidbody.transform.up * rightEngineThrust;
+
+        ui.leftEngineValue.text = leftEngineThrust.ToString();
+        ui.rightEngineValue.text = rightEngineThrust.ToString();
+    }
+}
